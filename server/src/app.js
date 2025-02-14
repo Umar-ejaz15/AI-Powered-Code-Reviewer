d9976df61057dc122a9acf07962071563febf30c
umar ejaz <umar.senpaiiii@gmail.com>
2025-02-14 23:38:13 +0500

    v7

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,11 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // const aiRoutes = require('./routes/ai.routes');
 app.get("/", (req, res) => {
   res.send("Hello World!");
