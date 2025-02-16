e4d8b70bd0c5c61bd0417d200091adf91da58748
umar ejaz <umar.senpaiiii@gmail.com>
2025-02-17 00:47:36 +0500

    v19

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,15 @@ const app = express();
 app.use(
     cors({
       origin: ["https://ai-powered-code-reviewer-silk.vercel.app"], // Allow specific frontend
-      methods: ["GET", "POST", "PUT", "DELETE"], // Specify methods
-    //   allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow headers
+    //   methods: ["GET", "POST", "PUT", "DELETE",""], // Specify methods
+    methods: ["GET", "POST", "OPTIONS"],
+
+      allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow headers
     //   credentials: true, // Allow cookies if needed
     })
   );
+  app.options("*", cors()); // Handles preflight requests
+
   
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
